b7129d7a7fdb6bc52de25af85fa7864714b77cef
attilaszuts <45080765+attilaszuts@users.noreply.github.com>
2023-03-21 17:17:46 +0100

    create utils.py

---
+++ code/utils.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+DATASETS = [
+    "sumanthvrao/fakenewsdataset",
+    "clmentbisaillon/fake-and-real-news-dataset"
+]
+COMPETITION = "fake-news"
+DATASET_NAMES = [
+    "fake-and-real-news-dataset",
+    "fake-news",
+    "fakenewsdataset"
+]
+
+ROOT_DIR = Path(__file__).parent.parent

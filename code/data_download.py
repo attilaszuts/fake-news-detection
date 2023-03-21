76f6111d076665a01618815901a9ec4d532062c1
attilaszuts <45080765+attilaszuts@users.noreply.github.com>
2023-03-21 17:17:58 +0100

    create data download script

---
+++ code/data_download.py
@@ -0,0 +1,27 @@
+import zipfile
+
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+from utils import ROOT_DIR, COMPETITION, DATASETS
+
+# Define competition, dataset and file names
+compressed_source = f"{ROOT_DIR}/data/compressed"
+files = [
+    "fake-and-real-news-dataset.zip",
+    "fake-news.zip",
+    "fakenewsdataset.zip"
+]
+extracted_source = f"{ROOT_DIR}/data/extracted"
+
+# Download data from Kaggle
+api = KaggleApi()
+api.authenticate()
+
+for d in datasets:
+    api.dataset_download_files(dataset=d, path=compressed_source)
+api.competition_download_files(competition=COMPETITION, path=compressed_source)
+
+# Extract data from zipfiles
+for f in files:
+    with zipfile.ZipFile(f"{compressed_source}/{f}", 'r') as zip_ref:
+        zip_ref.extractall(f"{extracted_source}/{f}")
